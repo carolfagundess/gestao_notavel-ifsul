cd23c8a5f2e823dc66da8721882cea3b78f1db06
Carol Fagundes <carolinarftk@gmail.com>
2025-09-24 22:43:22 -0300

    CRUD usuarioDAO codado

--- src/main/java/br/com/gestaonotavel/gestao_notavel/ifsul/dao/UsuarioDAO.java
+++ src/main/java/br/com/gestaonotavel/gestao_notavel/ifsul/dao/UsuarioDAO.java
@@ -1,10 +1,65 @@
 
 package br.com.gestaonotavel.gestao_notavel.ifsul.dao;
 
+import br.com.gestaonotavel.gestao_notavel.ifsul.model.Usuario;
+import br.com.gestaonotavel.gestao_notavel.ifsul.util.JpaUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.List;
+
 /**
  *
  * @author carol
  */
 public class UsuarioDAO {
-    
+
+    public Usuario salvarUsuario(Usuario usuario) {
+        EntityManager em = JpaUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            Usuario usuarioSalvo = em.merge(usuario);
+            tx.commit();
+            return usuarioSalvo;
+        } catch (Exception ex) {
+            tx.rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+
+    public Usuario buscarPorId(Long id) {
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            return em.find(Usuario.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Usuario> listarTodos() {
+        EntityManager em = JpaUtil.getEntityManager();
+        try {
+            return em.createQuery("SELECT u FROM Usuario u", Usuario.class).getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void remover(Usuario usuario) {
+        EntityManager em = JpaUtil.getEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            em.remove(em.merge(usuario));
+            tx.commit();
+        }catch (Exception ex) {
+            tx.rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
 }
